8e06554732684ed8e267b2d2d422605e26dbe7c0
Jerry CHAN <103566524+jerrychan88@users.noreply.github.com>
2024-09-22 22:45:29 -0400

    etape 5

--- Seance4/basic-android-kotlin-compose-training-woof-starter/app/src/main/java/com/example/woof/ui/theme/Shape.kt
+++ Seance4/basic-android-kotlin-compose-training-woof-starter/app/src/main/java/com/example/woof/ui/theme/Shape.kt
@@ -20,5 +20,6 @@ import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
-    small = RoundedCornerShape(50.dp),
+    small = RoundedCornerShape(90.dp),
+    medium = RoundedCornerShape(bottomStart = 16.dp, topEnd = 16.dp)
 )
